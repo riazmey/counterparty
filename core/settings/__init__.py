d77ebea41a4e1d9e47e204359cae0e07e87a8cf8
Иван Радаев <i.radaev@ltlog.ru>
2025-04-11 09:44:34 +0300

    Edit add print view environments vars

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -5,3 +5,12 @@ try:
     from .local import * # type: ignore
 except ImportError:
     from .environ import *
+
+print('Debug view environments vars')
+if DEBUG:
+    print(f'    SECRET_KEY: {SECRET_KEY}')
+    print(f'    DEBUG: {DEBUG}')
+    print(f'    ALLOWED_HOSTS: {ALLOWED_HOSTS}')
+    print(f'    DATABASES: {DATABASES}')
+else:
+    print(f'    DEBUG: {DEBUG}')
